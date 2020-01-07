d85f6a2a3ce65621c46a6c32a184d4bf919d52db
Vitaly Buka <vitalybuka@chromium.org>
2020-01-07 07:24:48 +0000

    Disable variable initialization in sandbox_nt_util.cc
    This is working version of crrev.com/c/1966357
    Previous fix does not work.
    
    Bug: 977230
    Change-Id: I859e57856bd7fb07b7587f8f80f08c7feb24b69a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1973353
    Commit-Queue: Vitaly Buka <vitalybuka@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#728833}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 1775fe15dcbc6fd069810a058a8c40f54050a692

--- win/src/sandbox_nt_util.cc
+++ win/src/sandbox_nt_util.cc
@@ -9,6 +9,7 @@
 
 #include <string>
 
+#include "base/compiler_specific.h"
 #include "base/win/pe_image.h"
 #include "sandbox/win/src/sandbox_factory.h"
 #include "sandbox/win/src/target_services.h"
@@ -58,18 +59,8 @@ void* AllocateNearTo(void* source, size_t size) {
   const char* top_address = base + kMaxSize;
 
   while (base < top_address) {
-    // Initialize all fields to avoid memset with init_stack_vars = true.
-    // "= {}" or  -ftrivial-auto-var-init=pattern may insert memset or memcpy
-    // here. However if memset was used here even "init_stack_vars = false
-    // is_debug = true" on x86_64 crashes in GPU process.
-    MEMORY_BASIC_INFORMATION mem_info;
-    mem_info.BaseAddress = nullptr;
-    mem_info.AllocationBase = nullptr;
-    mem_info.AllocationProtect = 0;
-    mem_info.RegionSize = 0;
-    mem_info.State = 0;
-    mem_info.Protect = 0;
-    mem_info.Type = 0;
+    // Avoid memset inserted by -ftrivial-auto-var-init=pattern.
+    STACK_UNINITIALIZED MEMORY_BASIC_INFORMATION mem_info;
     NTSTATUS status =
         g_nt.QueryVirtualMemory(NtCurrentProcess, base, MemoryBasicInformation,
                                 &mem_info, sizeof(mem_info), nullptr);
